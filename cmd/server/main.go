3597e5ab9f6da67aa8e1b9669790980c38398dd5
Aneeshie <aneeshdas556@gmail.com>
2026-03-07 21:01:07 +0530

    feat: initial config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/Aneeshie/cpp-judge/internal/config"
+)
 
 func main(){
-	fmt.Println("hello world")
+
+
 }
---
+++ internal/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+	"os"
+)
+
+type Config struct {
+	Port string
+	DatabaseURL string
+}
+
+
+func Load() *Config{
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		log.Fatal("database url not found")
+		return nil
+	}
+
+	return &Config{
+		Port: port,
+		DatabaseURL: databaseURL,
+	}
+}
